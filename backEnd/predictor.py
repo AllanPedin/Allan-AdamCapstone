37c4ae69467f892f1c935f09c6a4347a26d1fe4f
Allan Pedin <allan.pedin.17@cnu.edu>
2020-03-01 20:36:27 -0500

    Neural net model code

--- backEnd/predictor.py
+++ backEnd/predictor.py
@@ -10,4 +10,19 @@ import matplotlib.pyplot as plt
 
 # print(tf.__version__)
 
-# dataSet = [1]
+# fashion_mnist = keras.datasets.fashion_mnist
+
+# (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+train_games = [1,2] #not sure what the format this is yet
+train_labels = [1,2]
+test_games = [1,2]
+test_labels = [1,2]
+
+class_names = ['Win', 'Lose']#left side wins or left side loses
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='sigmoid'),#activation function sigmoid is a classic
+    keras.layers.Dense(10, activation='softmax')#dunno what a softma is
+])
