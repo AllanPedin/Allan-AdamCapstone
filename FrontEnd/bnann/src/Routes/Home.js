b029fa57bf8b7319646f0930b6f64e2ee62dcac5
Allan <allan.pedin.17@cnu.edu>
2020-03-24 02:43:25 -0400

    front end now handles an array of games

--- FrontEnd/bnann/src/Routes/Home.js
+++ FrontEnd/bnann/src/Routes/Home.js
@@ -1,30 +1,72 @@
 import React from 'react';
 import './Home.css'
+let games = [
+    {
+        teamNameRight: "Milwaukee Bucks",
+        teamNameLeft: "Houston Rockets",
+        Winner: "left"
+    },    
+    {
+        teamNameRight: "LA Clippers",
+        teamNameLeft: "Washington Wizards",
+        Winner: "left"
+    },
+    {
+        teamNameRight: "Dallas Mavericks",
+        teamNameLeft: "New Orleans Pelicans",
+        Winner: "left"
+    },
+    {
+        teamNameRight: "Los Angeles Lakers",
+        teamNameLeft: "Portland Trail Blazers",
+        Winner: "left"
+    },
+    {
+        teamNameRight: "Minnesota Timberwolves",
+        teamNameLeft: "San Antonio Spurs",
+        Winner: "left"
+    },
+    {
+        teamNameRight: "Boston Celtics",
+        teamNameLeft: "Toronto Raptors",
+        Winner: "left"
+    },
+    {
+        teamNameRight: "Memphis Grizzlies",
+        teamNameLeft: "Phoenix Suns",
+        Winner: "left"
+    },
+    {
+        teamNameRight: "Miami Heat",
+        teamNameLeft: "Atlanta Hawks",
+        Winner: "left"
+    },
+    {
+        teamNameRight: "Utah Jazz",
+        teamNameLeft: "Brooklyn Nets",
+        Winner: "left"
+    },
+    {
+        teamNameRight: "Oklahoma City Thunder",
+        teamNameLeft: "Denver Nuggets",
+        Winner: "left"
+    },
+    {
+        teamNameRight: "Philadelphia 76ers",
+        teamNameLeft: "Indiana Pacers",
+        Winner: "left"
+    }
+]
 export default function Home() {
     return <div className='gameList'>
-        <div className='game'>
-            <div className='teamLeft'>Team1</div>
-            <div className='teamRight'>Team2</div>
-        </div>
-        <div className='game'>
-            <div className='teamLeft'>Team1</div>
-            <div className='teamRight'>Team2</div>
-        </div>
-        <div className='game'>
-            <div className='teamLeft'>Team1</div>
-            <div className='teamRight'>Team2</div>
-        </div>
-        <div className='game'>
-            <div className='teamLeft'>Team1</div>
-            <div className='teamRight'>Team2</div>
-        </div>
-        <div className='game'>
-            <div className='teamLeft'>Team1</div>
-            <div className='teamRight'>Team2</div>
-        </div>
-        <div className='game'>
-            <div className='teamLeft'>Team1</div>
-            <div className='teamRight'>Team2</div>
-        </div>
+        {
+            games.map((game, index)=>{
+                return  (
+                <div className='game'>
+                    <div className='teamLeft'>{game.teamNameLeft}</div>
+                    <div className='teamRight'>{game.teamNameRight}</div>
+                </div>)
+            })
+        }
     </div>;
 }
