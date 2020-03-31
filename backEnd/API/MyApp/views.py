c8a5a54f59da6ca0f80f73f9ccf7212d4b4217f3
Allan <allan.pedin.17@cnu.edu>
2020-03-31 02:58:55 -0400

    getting some mock games from the backend

--- backEnd/API/MyApp/views.py
+++ backEnd/API/MyApp/views.py
@@ -12,14 +12,64 @@ import json
 def gamePredictions(request):
     try:
         data = {
-                'games':[ #call to get predictions from NN
-                    {
-                        'teamName1': 'dogs',
-                        'teamName2': 'cats',
-                        'winnerIsTeam1' : True
-                    }
-                ]
-            }
+            'games':[ #call to get predictions from NN
+                {
+                    "teamName1": "Milwaukee Bucks",
+                    "teamName2": "Houston Rockets",
+                    "winnerIsTeam1": True
+                },    
+                {
+                    "teamName1": "LA Clippers",
+                    "teamName2": "Washington Wizards",
+                    "winnerIsTeam1": True
+                },
+                {
+                    "teamName1": "Dallas Mavericks",
+                    "teamName2": "New Orleans Pelicans",
+                    "winnerIsTeam1": True
+                },
+                {
+                    "teamName1": "Los Angeles Lakers",
+                    "teamName2": "Portland Trail Blazers",
+                    "winnerIsTeam1": True
+                },
+                {
+                    "teamName1": "Minnesota Timberwolves",
+                    "teamName2": "San Antonio Spurs",
+                    "winnerIsTeam1": True
+                },
+                {
+                    "teamName1": "Boston Celtics",
+                    "teamName2": "Toronto Raptors",
+                    "winnerIsTeam1": True
+                },
+                {
+                    "teamName1": "Memphis Grizzlies",
+                    "teamName2": "Phoenix Suns",
+                    "winnerIsTeam1": True
+                },
+                {
+                    "teamName1": "Miami Heat",
+                    "teamName2": "Atlanta Hawks",
+                    "winnerIsTeam1": True
+                },
+                {
+                    "teamName1": "Utah Jazz",
+                    "teamName2": "Brooklyn Nets",
+                    "winnerIsTeam1": True
+                },
+                {
+                    "teamName1": "Oklahoma City Thunder",
+                    "teamName2": "Denver Nuggets",
+                    "winnerIsTeam1": True
+                },
+                {
+                    "teamName1": "Philadelphia 76ers",
+                    "teamName2": "Indiana Pacers",
+                    "winnerIsTeam1": True
+                }
+            ]
+        }
         return JsonResponse(data)
     except ValueError as e:
         return Response(e.args[0],status.HTTP_400_BAD_REQUEST)
