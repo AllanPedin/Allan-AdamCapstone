ab585876b11d502f757994daaee1460c36281f96
Allan <allan.pedin.17@cnu.edu>
2020-03-31 03:01:09 -0400

    front end now getting mock data from the backend

--- FrontEnd/bnann/src/Routes/Home.js
+++ FrontEnd/bnann/src/Routes/Home.js
@@ -1,74 +1,34 @@
 import React from 'react';
 import './Home.css'
-let games = [
-    {
-        teamNameRight: "Milwaukee Bucks",
-        teamNameLeft: "Houston Rockets",
-        Winner: "left"
-    },    
-    {
-        teamNameRight: "LA Clippers",
-        teamNameLeft: "Washington Wizards",
-        Winner: "left"
-    },
-    {
-        teamNameRight: "Dallas Mavericks",
-        teamNameLeft: "New Orleans Pelicans",
-        Winner: "left"
-    },
-    {
-        teamNameRight: "Los Angeles Lakers",
-        teamNameLeft: "Portland Trail Blazers",
-        Winner: "left"
-    },
-    {
-        teamNameRight: "Minnesota Timberwolves",
-        teamNameLeft: "San Antonio Spurs",
-        Winner: "left"
-    },
-    {
-        teamNameRight: "Boston Celtics",
-        teamNameLeft: "Toronto Raptors",
-        Winner: "left"
-    },
-    {
-        teamNameRight: "Memphis Grizzlies",
-        teamNameLeft: "Phoenix Suns",
-        Winner: "left"
-    },
-    {
-        teamNameRight: "Miami Heat",
-        teamNameLeft: "Atlanta Hawks",
-        Winner: "left"
-    },
-    {
-        teamNameRight: "Utah Jazz",
-        teamNameLeft: "Brooklyn Nets",
-        Winner: "left"
-    },
-    {
-        teamNameRight: "Oklahoma City Thunder",
-        teamNameLeft: "Denver Nuggets",
-        Winner: "left"
-    },
-    {
-        teamNameRight: "Philadelphia 76ers",
-        teamNameLeft: "Indiana Pacers",
-        Winner: "left"
+const axios = require('axios')
+const constants = require("../constants")
+
+export default class Home extends React.Component {
+    state = {
+      games: []
     }
-]
-export default function Home() {
-    return <div className='gameList'>
+    componentDidMount() {
+        axios.get(constants.API_ENDPOINT+ "gamepredictions/").then((response)=>{
+            console.log(response)
+            let games = response.data.games;
+            this.setState({ games });
+        },(error)=>{
+            console.log(error);
+        })
+    }
+    render() {
+        return <div className = 'gameList'>
         {
-            games.map((game, index)=>{
+            this.state.games.map((game, index)=>{
                 return (
-                    <div className={index!==games.length-1 ? "game" : "game last"}>
-                        <div className='teamLeft'>{game.teamNameLeft}</div>
+                    <div className={index!==this.state.games.length-1 ? "game" : "game last"}>
+                        <div className='teamLeft'>{game.teamName1}</div>
                         <div className="vs">vs</div>
-                        <div className='teamRight'>{game.teamNameRight}</div>
+                        <div className='teamRight'>{game.teamName2}</div>
                     </div>
                 )
             })
         }
     </div>;
-}
+    }
+  }
