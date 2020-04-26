e016047705634decf0477d7131e9578c1c4ac99c
allan <allan.pedin.17@cnu.edu>
2020-04-25 20:42:42 -0400

    cleaned up file

--- backEnd/dataAverager.py
+++ backEnd/dataAverager.py
@@ -1,39 +1,7 @@
 import csv
-def getFileString(team):#team = 3 character string team name
-    fileDict  = {# no switch cases in python apperently
-        "ATL": "AtlantaHawksgamelog.csv",
-        "BOS": "BostonCelticsgamelog.csv",
-        "BKN": "BrooklynNetsgamelog.csv",
-        "CHA": "CharlotteHornetsgamelog.csv",
-        "CHI": "ChicagoBullsgamelog.csv",
-        "CLE": "ClevelandCavaliersgamelog.csv",
-        "DAL": "DallasMavericksgamelog.csv",
-        "DEN": "DenverNuggetsgamelog.csv",
-        "DET": "DetroitPistonsgamelog.csv",
-        "GSW": "GoldenStateWarriorsgamelog.csv",
-        "HOU": "HoustonRocketsgamelog.csv",
-        "IND": "IndianaPacersgamelog.csv",
-        "LAC": "LAClippersgamelog.csv",
-        "LAL": "LosAngelesLakersgamelog.csv",
-        "MEM": "MemphisGrizzliesgamelog.csv",
-        "MIA": "MiamiHeatgamelog.csv",
-        "MIL": "MilwaukeeBucksgamelog.csv",
-        "MIN": "MinnesotaTimberwolvesgamelog.csv",
-        "NOP": "NewOrleansPelicansgamelog.csv",
-        "NYK": "NewYorkKnicksgamelog.csv",
-        "OKC": "OklahomaCityThundergamelog.csv",
-        "ORL": "OrlandoMagicgamelog.csv",
-        "PHI": "Philadelphia76ersgamelog.csv",
-        "PHX": "PhoenixSunsgamelog.csv",
-        "POR": "PortlandTrailBlazersgamelog.csv",
-        "SAC": "SacramentoKingsgamelog.csv",
-        "SAS": "SanAntonioSpursgamelog.csv",
-        "TOR": "TorontoRaptorsgamelog.csv",
-        "UTA": "UtahJazzgamelog.csv",
-        "WAS": "WashingtonWizardsgamelog.csv"         
-    }
-    return "nba_web_scraper/teamGameLogs/" + fileDict[team]
-
+def debugLogger(log):
+    if False:
+        print(log)
 
 def addStats(previousStats, currentStats):
     newStats = []
@@ -82,23 +50,18 @@ for teamLog in teamLogs:
                     previousStats = addStats(previousStats, currentStats)
                 numGames += 1
             except Exception as e:
-                print("row is empty or column tag")#row is empty
+                debugLogger("row is empty or column tag")#row is empty
                 continue
                     
-for date, stats in dateteamStatsDict.items():
-    print(date, stats)
-    if(stats != None):
-        print(len(stats))
 
 def getStatsOfGame(gameString):
     team1 = gameString[15:18]
     team2 = gameString[::-1][:3][::-1]
     statString1 = gameString[:15] + team1 
     statString2 = gameString[:15] + team2 
-    print(statString1,statString2)
     return [dateteamStatsDict[statString1], dateteamStatsDict[statString2]]
 
-# average stats againsts average stats
+# average stats againsts average stats with winner
 games = [] #array of tuples first item contains array of 2 game stats, second item contains 1 or 0, 1 means first stat won
 for teamLog in teamLogs:
     with open("nba_web_scraper/teamGameLogs/" + teamLog , mode='r') as csv_file:
@@ -107,14 +70,12 @@ for teamLog in teamLogs:
                 gameString = row[0]
                 if gameString == "MATCHUP":
                     raise Exception("Header of csv")
-                print(gameString)
                 winner = 1
                 if row[1] == "L":
                     winner = 0
-                games.append((getStatsOfGame(gameString), winner))#replace 1 with winner of game
-                print(getStatsOfGame(gameString))
+                games.append((getStatsOfGame(gameString), winner))
             except Exception as e:
-                print("row is empty or column header")
+                debugLogger("row is empty or column header")
 
             # game =     
 
@@ -124,4 +85,3 @@ for game, winner in games:
     print(winner)
 # print(games)
 print(len(games))
-# #save game in csv
